a2e8b3d3c69450755252920d37b2b529b8123021
ajlopez <webmaster@ajlopez.com>
2016-04-19 06:29:29 -0300

    Create tile using string

--- test/tiles.js
+++ test/tiles.js
@@ -10,3 +10,13 @@ exports['create tile with colors'] = function (test) {
     test.equal(tile.west(), 2);
     test.equal(tile.south(), 3);
 };
+
+exports['create tile using string'] = function (test) {
+    var tile = wt.tile("0123");
+    
+    test.ok(tile);
+    test.equal(tile.east(), 0);
+    test.equal(tile.north(), 1);
+    test.equal(tile.west(), 2);
+    test.equal(tile.south(), 3);
+};
