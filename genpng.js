58ea913976394dde3f4cdec9c0cecd4c484a21fb
ajlopez <webmaster@ajlopez.com>
2016-06-05 10:15:14 -0300

    Refactor generate png sample

--- genpng.js
+++ genpng.js
@@ -3,21 +3,26 @@ var PNG = require('node-png').PNG;
 
 var png = new PNG();
 
-png.width = 100;
-png.height = 100;
+var size = 100;
 
-var data = new Array(100 * 100 * 4);
+png.width = size;
+png.height = size;
 
-for (var k = 0; k < 40000; k += 4) {
+var totalsize = size * size * 4;
+
+var data = new Array(totalsize);
+
+for (var k = 0; k < totalsize; k += 4) {
     data[k] = 0;
     data[k+1] = 0;
     data[k+2] = 0;
     data[k+3] = 255;
 }
 
-for (var y = 1; y < 99; y++)
-    for (var x = y; x < 100 - y; x++) {
-        var k = (y * 100 + x) * 4;
+// north: yellow
+for (var y = 1; y <= size - 1; y++)
+    for (var x = y; x < size - y; x++) {
+        var k = (y * size + x) * 4;
         
         data[k] = 255;
         data[k+1] = 255;
@@ -25,9 +30,10 @@ for (var y = 1; y < 99; y++)
         data[k+3] = 255;
     }
 
-for (var y = 1; y < 99; y++)
+// west: red
+for (var y = 1; y <= size - 1; y++)
     for (var x = 1; x < y; x++) {
-        var k = (y * 100 + x) * 4;
+        var k = (y * size + x) * 4;
         
         data[k] = 255;
         data[k+1] = 0;
@@ -35,9 +41,10 @@ for (var y = 1; y < 99; y++)
         data[k+3] = 255;
     }
 
-for (var y = 1; y < 99; y++)
+// east: blue
+for (var y = 1; y <= size - 1; y++)
     for (var x = 1; x < y; x++) {
-        var k = (y * 100 + 99 - x) * 4;
+        var k = (y * size + size - 1 - x) * 4;
         
         data[k] = 0;
         data[k+1] = 0;
@@ -45,9 +52,10 @@ for (var y = 1; y < 99; y++)
         data[k+3] = 255;
     }
 
-for (var y = 1; y < 99; y++)
-    for (var x = y; x < 100 - y; x++) {
-        var k = ((100 - y) * 100 + x) * 4;
+// south: lime
+for (var y = 1; y <= size - 1; y++)
+    for (var x = y; x < size - y; x++) {
+        var k = ((size - y) * size + x) * 4;
         
         data[k] = 0;
         data[k+1] = 255;
