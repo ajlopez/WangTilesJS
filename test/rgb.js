1d8ce3f3604ad764a99d3e523540a99f1e57b7fd
ajlopez <webmaster@ajlopez.com>
2016-06-05 09:37:40 -0300

    RGB colors, first test

---
+++ test/rgb.js
@@ -0,0 +1,16 @@
+
+var wt = require('..');
+
+exports['get name colors as rgb array'] = function (test) {
+    test.deepEqual(wt.rgb('red'), [255, 0, 0]);
+    test.deepEqual(wt.rgb('lime'), [0, 255, 0]);
+    test.deepEqual(wt.rgb('blue'), [0, 0, 255]);
+
+    test.deepEqual(wt.rgb('yellow'), [255, 255, 0]);
+    test.deepEqual(wt.rgb('magenta'), [255, 0, 255]);
+    test.deepEqual(wt.rgb('cyan'), [0, 255, 255]);
+
+    test.deepEqual(wt.rgb('white'), [255, 255, 255]);
+    test.deepEqual(wt.rgb('black'), [0, 0, 0]);
+};
+
