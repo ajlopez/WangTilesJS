af56387963b563bb3a48378a6a7b2064e01fec97
ajlopez <webmaster@ajlopez.com>
2016-06-10 05:53:39 -0300

    Load plane with tile

--- test/load.js
+++ test/load.js
@@ -8,3 +8,32 @@ exports['load plane'] = function (test) {
     test.equal(plane.width(), 10);
     test.equal(plane.height(), 5);
 };
+
+exports['load plane with tile'] = function (test) {
+    var plane = wt.loadPlane({ 
+        width: 10, 
+        height: 5,
+        positions: [
+            {
+                x: 1,
+                y: 2,
+                tile: [0, 1, 2, 3]
+            }
+        ]
+    });
+    
+    test.ok(plane);
+    test.equal(plane.width(), 10);
+    test.equal(plane.height(), 5);
+    
+    test.equal(plane.get(0, 0), null);
+    
+    var result = plane.get(1, 2);
+    
+    test.ok(result);
+    test.equal(result.east(), 0);
+    test.equal(result.north(), 1);
+    test.equal(result.west(), 2);
+    test.equal(result.south(), 3);
+};
+
